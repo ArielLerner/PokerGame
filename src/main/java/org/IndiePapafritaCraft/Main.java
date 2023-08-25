18d23aeb9744d8bfab1d82b23e054cbe25286919
ArielLerner <139831545+ArielLerner@users.noreply.github.com>
2023-08-25 15:10:45 -0300

    MetodosDeApuesta Commit

--- src/main/java/org/IndiePapafritaCraft/Main.java
+++ src/main/java/org/IndiePapafritaCraft/Main.java
@@ -1,12 +1,14 @@
 package org.IndiePapafritaCraft;
 
 import jdk.jshell.execution.Util;
+import org.IndiePapafritaCraft.ClasesDeLaCpu.UtilidadesCpu.MetodosDeApuestas;
 import org.IndiePapafritaCraft.ClasesJuegoPoker.JuegoPoker;
 import org.IndiePapafritaCraft.ClasesJuegoPoker.UtilidadesJuegoPoker;
 import org.IndiePapafritaCraft.ClasesJuegoPoker.UtilidadesJuegoPokerTerminal;
 
 public class Main {
     public static void main(String[] args) {
-        JuegoPoker.crearJuegoTerminal();
+        MetodosDeApuestas.anotarCantManosJugadas(5);
+        System.out.println("listo");
     }
     }
